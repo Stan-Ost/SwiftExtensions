76ecafe3bfdcd15d9b22db007da9b5f67bc040a3
Stan Ostrovskiy <stasost@gmail.com>
2023-01-06 21:04:48 -0500

    add bundle extension

--- Sources/SwiftExtensions/Bundle.swift
+++ Sources/SwiftExtensions/Bundle.swift
@@ -4,5 +4,9 @@ public extension Bundle {
     var appVersion: String {
         return object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
     }
+    
+    var displayName: String {
+        return object(forInfoDictionaryKey: "CFBundleDisplayName") as! String
+    }
 }
 
